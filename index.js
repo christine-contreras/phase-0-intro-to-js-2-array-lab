7630536988a76ae41288708e38f4533b5321d8a6
Christine Contreras <christineec23@yahoo.com>
2021-03-11 19:41:23 -0600

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift(name);
+}
+
+
+function appendCat(name) {
+    const moreCats = [...cats, name];
+    return moreCats;
+}
+
+
+function prependCat(name) {
+    const moreCats = [name, ...cats];
+    return moreCats;
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
